2177fe11f24e576b04df7a4c5f88a1145aaaec80
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2023-04-05 01:34:57 -0500

    se agrega funcion resta

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ int suma(int a, int b){
     return a+b;
 }
 
+int resta(int a, int b){
+    return a-b;
+}
+
 int main(){
 
     printf("%i \n", suma(3, 9));
+    printf("%i \n", resta(10, 2)); 
     system("pause");
 }
